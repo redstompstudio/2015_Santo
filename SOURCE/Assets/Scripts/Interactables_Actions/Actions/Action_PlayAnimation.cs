6a6b70d3e4e0f51c27a63fec0add8204b78b1aab
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-06-18 15:23:03 -0300

    - Added props on level.
    Signed-off-by: Arthur Fukushima <arthuryoshinori@gmail.com>

---
+++ SOURCE/Assets/Scripts/Interactables_Actions/Actions/Action_PlayAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Action_PlayAnimation : BaseAction 
+{
+	private Animator animator;
+
+	public string stateName;
+	public float transionDuration = 0.1f;
+
+	protected override void Awake ()
+	{
+		base.Awake ();
+
+		if (animator == null)
+			animator = GetComponent<Animator> ();
+	}
+
+	public override void StartAction ()
+	{
+		animator.CrossFade (stateName, transionDuration);
+	}
+}
---
+++ SOURCE/Assets/Scripts/Interactables_Actions/Actions/OpenSkullDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenSkullDoor : BaseAction
+{
+	public Action_PlayAnimation doorAnimation;
+
+	public override void StartAction ()
+	{
+		base.StartAction ();
+
+		StartCoroutine (OpenDoor ());
+	}
+
+	private IEnumerator OpenDoor()
+	{
+		yield return new WaitForSeconds (actionDelay);
+
+		if(doorAnimation != null)
+			doorAnimation.StartAction ();
+	}
+}
+
