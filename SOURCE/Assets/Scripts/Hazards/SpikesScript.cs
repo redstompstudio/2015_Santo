fb9d9ca15ce6d057be67e31a35816e6b59fb8752
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-06-19 21:42:31 -0300

    - Spikes - Player : Receive Damage Anim. - After a Slide, the player no longer go to the Idle State if Crouch button is pressed.
    Signed-off-by: Arthur Fukushima <arthuryoshinori@gmail.com>

---
+++ SOURCE/Assets/Scripts/Hazards/SpikesScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpikesScript : MonoBehaviour 
+{
+	private List<BaseActor> actorsInside = new List<BaseActor>();
+
+	public int dps;
+	public TriggerHandler trigger;
+
+	private float lastDamageTime;
+
+	private void Awake()
+	{
+		trigger.onTriggerEnterCallback += OnEnter;
+		trigger.onTriggerExitCallback += OnExit;
+	}
+
+	private void Update()
+	{
+		if (lastDamageTime >= 1.0f) {
+			foreach (BaseActor actor in actorsInside) {
+				actor.ReceiveDamage (null, dps, DAMAGE_TYPE.PIERCE, actor.Position);
+			}
+
+			lastDamageTime = 0.0f;
+		}
+
+		lastDamageTime += Time.deltaTime;
+	}
+
+	void OnEnter(Collider pOther)
+	{
+		BaseActor actor = pOther.GetComponent<BaseActor> ();
+
+		if(actor != null && !actorsInside.Contains(actor))
+		{
+			actorsInside.Add (actor);
+			lastDamageTime = 1.0f;
+		}
+	}
+
+	void OnExit(Collider pOther)
+	{
+		BaseActor actor = pOther.GetComponent<BaseActor> ();
+
+		if(actor != null && actorsInside.Contains(actor))
+		{
+			actorsInside.Remove (actor);
+		}
+	}
+}
